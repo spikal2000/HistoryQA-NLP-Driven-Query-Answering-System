4b39f909150d33b38889ed8a4abbd2258e04bae6
spikal2000 <73543919+spikal2000@users.noreply.github.com>
2023-06-09 12:30:46 +0300

    Update generativeAi.py

--- generativeAi.py
+++ generativeAi.py
@@ -7,8 +7,12 @@ Created on Thu Jun  8 23:06:00 2023
 from IRmodel import TextSearchEngine
 import openai
 
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
 search_engine = TextSearchEngine('output.csv')
-query = 'what can you tell me about Greece'
+query = 'what is the Berlin wall? '
 results = search_engine.search(query, 5)
 # for result in results:
 #     print(result)
@@ -17,6 +21,15 @@ doc = [result['body_text'] for result in results]
 doc_text = ' '.join(doc)
 
 openai.api_key = ''
+#Bert
+# Load the model
+model = SentenceTransformer('all-MiniLM-L6-v2')
+# Calculate embedding for the query
+query_embedding = model.encode([query])
+
+all_generated_answers = []
+# Calculate cosine similarity between the query and each generated answer
+similarities = []
 
 
 prompts = [
@@ -32,7 +45,7 @@ for prompt in prompts:
     
     response = openai.ChatCompletion.create(
             model = 'gpt-3.5-turbo',
-            messages=[{"role": "system", "content": "You are a friendly and helpful teaching assistant. You explain concepts about world history based on the doc provided."},
+            messages=[{"role": "system", "content": "You are a friendly and helpful teaching assistant. You explain concepts about world history only based on the doc provided."},
                       {"role": "system", "content": f"{doc_text}"},
                       {"role": "user", "content": f"{full_prompt}"},],
             max_tokens = 50,
@@ -40,5 +53,17 @@ for prompt in prompts:
             temperature = 0.7
         )
     
-generated_answer = response['choices'][0]['message']['content']
+    generated_answer = response['choices'][0]['message']['content']
+    all_generated_answers.append(generated_answer)
+    
+    # Calculate embedding for the answer
+    answer_embedding = model.encode([generated_answer])
+    
+    # Calculate and save the cosine similarity
+    similarity = cosine_similarity(query_embedding, answer_embedding)
+    similarities.append(similarity[0][0])
+
+# Find the answer with the highest similarity
+best_answer_index = np.argmax(similarities)
+best_answer = all_generated_answers[best_answer_index]
 
