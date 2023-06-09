35801a8657ac7a67bbe0aaad086ca75f97d75a1a
spikal2000 <73543919+spikal2000@users.noreply.github.com>
2023-06-09 11:30:26 +0300

    Update generativeAi.py

--- generativeAi.py
+++ generativeAi.py
@@ -16,27 +16,29 @@ doc = [result['body_text'] for result in results]
 
 doc_text = ' '.join(doc)
 
-openai.api_key = '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'
+openai.api_key = ''
 
 
 prompts = [
-    "Explain the specific events"
+    "Discuss the historical significance and impact of",
+    "Explain the major factors that contributed to",
+    "Explore the key individuals who played a significant role in",
+    "Describe the social, political, and economic changes during"
     ]
-
 
 for prompt in prompts:
     
     full_prompt = f"{prompt} {query}"
     
-    response = openai.Completion.create(
+    response = openai.ChatCompletion.create(
             model = 'gpt-3.5-turbo',
-            prompt = full_prompt,
-            documents=doc_text,
-            max_tokens = 20,
+            messages=[{"role": "system", "content": "You are a friendly and helpful teaching assistant. You explain concepts about world history based on the doc provided."},
+                      {"role": "system", "content": f"{doc_text}"},
+                      {"role": "user", "content": f"{full_prompt}"},],
+            max_tokens = 50,
             n=1,
-            stop=None,
             temperature = 0.7
         )
     
-generated_answer = response.choices[0].message.content
+generated_answer = response['choices'][0]['message']['content']
 
